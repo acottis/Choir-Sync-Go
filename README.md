33ea46913c0777244989499727049a9fa99d9fd9
adam.cottis@outlook.com <adam.cottis@outlook.com>
2022-11-27 21:10:26 +0000

    Log in and local testing

---
+++ README.md
@@ -0,0 +1,11 @@
+# Choir Sync Go
+
+## Dev Setup
+
+* To authenticate to gcloud while testing locally run `gcloud auth application-default login`
+* To run server locally `go run main.go`
+* Default port is `8080` unless you set the `PORT` environment variable
+
+## Deploy
+
+1. `gcloud app deploy`
--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"cloud.google.com/go/storage"
 )
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-
+	// Get the port from the env, or default to port 8080
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
+
+	// Serve our static files to the root directory
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	// Our API endpoints
+	http.HandleFunc("/api", indexHandler)
+	http.HandleFunc("/api/v1/uploadfile", uploadFileHandler)
 
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -23,9 +31,20 @@ func main() {
 }
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
+	if req.URL.Path != "/api" {
 		http.NotFound(res, req)
 		return
 	}
 	fmt.Fprint(res, "Hello, World!")
+}
+
+func uploadFileHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "Hello, World!")
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
 }
