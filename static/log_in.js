9e14ce473ee7d889eefc58e89ed854b25faa0224
Naomi Russell <naomirussell@hotmail.co.uk>
2023-01-02 21:20:17 +0000

    moved some functions to add skeleton admin page

--- static/log_in.js
+++ static/log_in.js
@@ -1,5 +1,3 @@
-import {set_up_songs} from "/choose_song.js"
-
 const startdiv = document.getElementById("welcome")
 const maindiv = document.getElementById("requires_password")
 
@@ -7,6 +5,9 @@ const password_element = document.getElementById("password_entered")
 export let password_entered
 
 const form_authenticate = document.getElementById("form_authenticate")
+const admin = (form_authenticate.className == "admin")
+
+export let all_song_list = []
 
 const log_in = () => {
     password_entered = password_element.value
@@ -18,7 +19,9 @@ const show_page = async () => {
         if (password_correct) {
             startdiv.style.display = "none"
             maindiv.style.display = "block"
-            set_up_songs()
+            if (admin){
+                get_songs()
+            }
         }
     })
 }
@@ -28,6 +31,7 @@ const authenticate = (password_passed) => {
         const password_send = {password: password_passed}
 
         fetch(`/api/v1/auth`, {
+            //also pass whether we're signing in to admin or normal
             method: "post",
             headers: {
                 'Content-Type': 'application/json'
@@ -48,8 +52,32 @@ const authenticate = (password_passed) => {
 
 form_authenticate.addEventListener("submit", event  => {
     log_in()
-
-    //stop page refresh on form submit
-    event.preventDefault();
+    event.preventDefault(); //stop page refresh on form submit
     return false
 })
+
+export const get_songs = () => {
+    return new Promise (resolve =>{
+        const password_send = {password: password_entered}
+        fetch(`/api/v1/getsongs`, {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(password_send)
+        })
+        .then( res => {
+            if (res.status == 200){
+                res.json().then( res => {
+                    all_song_list = res
+                    resolve(true)
+                })
+            }else{
+                alert(
+                    "Server could not fetch songs, please contact administrator"
+                )
+                resolve(false)
+            }
+        })
+    })
+}
